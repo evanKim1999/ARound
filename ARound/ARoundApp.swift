60477415f3bcc03a8773a1ee75b82f50ee7a20b7
evanKim1999 <zone227@naver.com>
2025-06-20 19:16:51 +0900

    Initial Commit

---
+++ ARound/ARoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARoundApp.swift
+//  ARound
+//
+//  Created by eunchanKim on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct ARoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
